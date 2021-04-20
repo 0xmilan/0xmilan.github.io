804d5926de203a39ad7c1d8d53d8c3cce7206c0d
Milan Magyar <milangfx@gmail.com>
2021-04-20 23:57:38 +0200

    blog init

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,13 @@
+var store = [{
+        "title": "GGvulnz - How I hacked hundreds of companies through Google Groups",
+        "excerpt":"This logic flaw is the lovechild of two previous reports: One: How I hacked hundreds of companies through their helpdesk — aka “Ticket Trick” by Inti De Ceukelaire Two: Kenna Security’s research on the widespread Google Groups misconfiguration issue affecting thousands of instances, “Fortune 500 organizations; Hospitals; Universities and Colleges;...","categories": [],
+        "tags": [],
+        "url": "/2020/01/20/ggvulnz.html",
+        "teaser": null
+      },{
+        "title": "GGvulnz aftermath - How I forced Google to redesign Groups*",
+        "excerpt":"I’m a bit late with this post, but people were wondering if the methods described in the first GGVulnz post are still applicable today. In short, they are not. Google has released a new design of Groups which solved the main problems that enabled the exploit with the registration emails....","categories": [],
+        "tags": [],
+        "url": "/2021/04/20/ggvulnz-aftermath.html",
+        "teaser": null
+      }]
